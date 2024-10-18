54c6af190711937b90e1f2351542ce9e3d2d201b
Olafur Geirsson <olafurpg@gmail.com>
2024-10-18 16:18:11 +0200

    Example commit

--- examples/prompt-evals/test-dir/typescript/hello.ts
+++ examples/prompt-evals/test-dir/typescript/hello.ts
@@ -1,3 +1,13 @@
+interface Person {
+  name: string;
+  age: number;
+}
+function greeting(person: Person): string {
+  return `Bonjour, ${person.name}! Vous avez ${person.age} ans.`;
+}
+function add(a: number, x: string): number {
+  return "3".length - x.length - a;
+}
 export const hello = (name: string): string => {
   return `Hello ${name}`;
 };
