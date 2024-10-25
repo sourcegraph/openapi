dce5f3ee066c18380603f27d482d1a18cc52409f
Olafur Geirsson <olafurpg@gmail.com>
2024-10-25 15:28:15 +0200

    WIP

--- examples/prompt-evals/test-dir/typescript/hello.ts
+++ examples/prompt-evals/test-dir/typescript/hello.ts
@@ -2,6 +2,17 @@ interface Person {
   name: string;
   age: number;
 }
+
+interface Animal {
+  name: string;
+  species: string;
+}
+
+class Dog implements Animal {
+  name= "dog"
+  species = "mammal"
+}
+
 export const hello = (person: Person): string => {
   return `Hello ${person.name}, you are ${person.age} years old!`;
 };
