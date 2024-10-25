ef37cc4d9591752b461a32cec7a8a98b539873d2
Olafur Geirsson <olafurpg@gmail.com>
2024-10-25 15:25:54 +0200

    WIP

--- examples/prompt-evals/test-dir/typescript/hello.ts
+++ examples/prompt-evals/test-dir/typescript/hello.ts
@@ -1,3 +1,7 @@
-export const hello = (name: string): string => {
-  return `Hello ${name}`;
+interface Person {
+  name: string;
+  age: number;
+}
+export const hello = (person: Person): string => {
+  return `Hello ${person.name}, you are ${person.age} years old!`;
 };
