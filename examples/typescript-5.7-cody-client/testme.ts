a21ba70e4b49c79cd638cba631a412d5d1d23128
Olafur Geirsson <olafurpg@gmail.com>
2024-10-28 10:21:07 +0100

    WIP

---
+++ examples/typescript-5.7-cody-client/testme.ts
@@ -0,0 +1,17 @@
+interface ITestMe {
+  name: string;
+  age: number;
+  address: string;
+}
+
+const me: ITestMe = {
+  name: "John",
+  age: 30,
+  address: "",
+};
+console.log(me);
+function main() {
+  console.log("Hello World");
+}
+
+main();
